651743e45631515efc76b21149ee0abe2be0a037
ckshei <cks856@gmail.com>
2016-10-23 17:31:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const app = "I don't do much.";
+const digitalClock = {
+  time: Math.round(Date.now() / 1000), 
+  startTicking() {
+    setInterval(function(){this.time++}.bind(this), 1000);
+  }
+}
+
+function censor(word, string) {
+  var re = new RegExp(word, "g");
+  return string.replace(re, "BLEEP")
+}
+
+const violenceCensor = censor.bind(null, 'violence')
+
+const drugsCensor = censor.bind(null, 'drugs')
